111e8aca1136c349d41f3aa710fa92a252306ea8
Sam Lanning <sam@samlanning.com>
2021-07-27 08:07:38 +0100

    use viewport scale 1

--- pages/index.tsx
+++ pages/index.tsx
@@ -94,6 +94,7 @@ export default function Home() {
       <Head>
         <title>{NAME}</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
       </Head>
 
       <Header>
